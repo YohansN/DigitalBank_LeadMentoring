e62bc2152b0335b077919fc2f14ca62ad7c2d4f6
Yohans Nascimento <yohans.dev@gmail.com>
2022-11-11 17:29:05 -0300

    adicionando conexao ao banco de dados

---
+++ DigitalBankApi/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using DigitalBankApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalBankApi.Data
+{
+    public class Context : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-BKEG8HP;Initial Catalog=DigitalBankApi;Integrated Security=True");
+        }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<ContaBancaria> ContaBancaria { get; set; }
+        public DbSet<Transacao> Transacao { get; set; }
+    }
+}
