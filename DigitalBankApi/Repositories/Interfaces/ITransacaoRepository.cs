f955da25a45af3a6b737644a4418fa9560e3da42
Yohans Nascimento <yohans.dev@gmail.com>
2022-11-17 11:27:31 -0300

    adicionando Transacao Repository

---
+++ DigitalBankApi/Repositories/Interfaces/ITransacaoRepository.cs
@@ -0,0 +1,12 @@
+﻿using DigitalBankApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DigitalBankApi.Repositories.Interfaces
+{
+    public interface ITransacaoRepository
+    {
+        Task Add(Transacao transacao);
+        Task<List<Transacao>> GetAllByNumeroConta();
+    }
+}
---
+++ DigitalBankApi/Repositories/TransacaoRepository.cs
@@ -0,0 +1,29 @@
+﻿using DigitalBankApi.Data;
+using DigitalBankApi.Models;
+using DigitalBankApi.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DigitalBankApi.Repositories
+{
+    public class TransacaoRepository : ITransacaoRepository
+    {
+        private readonly Context _context;
+        public TransacaoRepository(Context context)
+        {
+            _context = context;
+        }
+
+        //Add - Adiciona uma nova transacao quando é chamada.
+        public async Task Add(Transacao transacao)
+        {
+            _context.Transacao.Add(transacao);
+            await _context.SaveChangesAsync();
+        }
+
+        //GetAllByNumeroConta - Retorna todas as transacoes de uma conta.
+        public async Task<List<Transacao>> GetAllByNumeroConta() => await _context.Transacao.ToListAsync();
+
+    }
+}
